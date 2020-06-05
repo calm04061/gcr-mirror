51e25a37d3c1af34937bde0c070d51e6cb21dbb4
Han Ding <calm0406@163.com>
2020-06-05 21:17:09 +0800

    mirr

--- mirror.sh
+++ mirror.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-for namespace in `ls`
+for site in `ls`
 do
-	if [ -d "$namespace" ];then
-		echo $namespace
+	if [ -d "$site" ];then
+		for namespace in `ls $site`
+		do
+			for image in `ls $site/$namespace`
+			do
+				for version in `ls $site/$namespace/$image`
+				do
+					echo $site/$namespace/$image:$version
+					docker pull $site/$namespace/$image:$version
+				done
+
+
+			done
+		done
 	fi
 done
