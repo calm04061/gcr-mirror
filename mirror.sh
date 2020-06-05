7335577ac5792bfe184183b834267e2913cfe13c
Han Ding <calm0406@163.com>
2020-06-05 21:35:42 +0800

    config image name

--- mirror.sh
+++ mirror.sh
@@ -10,8 +10,8 @@ do
 				do
 					echo $site/$namespace/$image:$version
 					docker pull $site/$namespace/$image:$version
-              				docker tag $site/$namespace/$image:$version docker.pkg.github.com/calm04061/image-mirror/$site/$namespace/$image:$version
-              				docker push docker.pkg.github.com/calm04061/image-mirror/$site/$namespace/$image:$version
+              				docker tag $site/$namespace/$image:$version docker.pkg.github.com/calm04061/image-mirror/$namespace-$image:$version
+              				docker push docker.pkg.github.com/calm04061/image-mirror/$namespace-$image:$version
 				done
 
 
