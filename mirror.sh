39acf3d9a08de1bffe35e4c0b92c7686b648e9a7
Han Ding <calm0406@163.com>
2020-06-05 22:21:27 +0800

    add aliyun

--- mirror.sh
+++ mirror.sh
@@ -12,6 +12,9 @@ do
 					docker pull $site/$namespace/$image:$version
               				docker tag $site/$namespace/$image:$version docker.pkg.github.com/calm04061/image-mirror/$namespace-$image:$version
               				docker push docker.pkg.github.com/calm04061/image-mirror/$namespace-$image:$version
+              				
+					docker tag $site/$namespace/$image:$version registry.cn-hangzhou.aliyuncs.com/$namespace/$image:$version
+              				docker push docker.registry.cn-hangzhou.aliyuncs.com/$namespace/$image:$version
 				done
 
 
